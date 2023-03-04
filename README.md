4ec98f465798459a073fb89ac3bd788fafbad3a7
giacThePhantom <giacomo.fantoni@studenti.unitn.it>
2023-03-04 18:20:21 +0100

    Added readme

---
+++ README.md
@@ -0,0 +1,8 @@
+### TODO
+- Define a configuration and format file
+- How to view the events in the file
+- Calendar view
+- Type of input prompts
+- Add or edit event (on file)
+- Shortcuts
+- Kanban board (based on tag and current status)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use chrono::{NaiveDate, Weekday};
+use inquire::{Text, validator::{StringValidator, Validation}, DateSelect};
+
+
 fn main() {
-    println!("Hello, world!");
+    let validator = |input: &str| if input.chars().count() > 140 {
+        Ok(Validation::Invalid("You're only allowed 140 characters.".into()))
+    } else {
+        Ok(Validation::Valid)
+    };
+
+    let status = Text::new("What are you thinking about?")
+        .with_validator(validator)
+        .prompt();
+
+    match status {
+        Ok(status) => println!("Your status is being published..."),
+        Err(err) => println!("Error while publishing your status: {}", err),
+    };
+
+    let date = DateSelect::new("When do you want to travel?")
+        .with_starting_date(NaiveDate::from_ymd(2021, 8, 1))
+        .with_min_date(NaiveDate::from_ymd(2021, 8, 1))
+        .with_max_date(NaiveDate::from_ymd(2021, 12, 31))
+        .with_week_start(Weekday::Mon)
+        .with_help_message("Possible flights will be displayed according to the selected date").with_vim_mode(true)
+        .prompt();
+
+    match date {
+        Ok(date) => println!("No flights available for this date. {:?}", date),
+        Err(_) => println!("There was an error in the system."),
+    }
+
 }
