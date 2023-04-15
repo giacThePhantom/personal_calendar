e17d5a5e33d96f6c48cc2f38424cd90e94d8fea5
giacThePhantom <giacomo.fantoni@studenti.unitn.it>
2023-04-16 01:33:06 +0200

    Playing with color and terminal information

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::io::{self, Write};
+use termion::{color, style, terminal_size};
 
 
 mod event;
@@ -10,4 +12,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let events = read_data::get_events(&args[1]);
     dbg!(events);
+    println!("{}Red", color::Fg(color::Red));
+    println!("{}Blue", color::Fg(color::Blue));
+    println!("{}Blue'n'Bold{}", style::Bold, style::Reset);
+    println!("{}Just plain italic", style::Italic);
+    let (n_col, _) = terminal_size().unwrap();
+    for i in 0..n_col {
+        print!("{} ", color::Bg(color::Red));
+    }
+    io::stdout().flush().unwrap();
 }
